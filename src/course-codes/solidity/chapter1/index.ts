7edc640f689dce8d1b0279ac9f607aea8cf20e5f
jonassunandar <jonassunandar@yahoo.com>
2020-06-22 19:20:49 +0700

    add solidity course

---
+++ src/course-codes/solidity/chapter1/index.ts
@@ -0,0 +1,183 @@
+import { ILessonCode, ChapterCodeType } from '../../../typings';
+
+const l1: ILessonCode = {
+  initialCode: `(* Tulis versi Solidity disini. *)
+
+(* Deklarasi kontrak disini. *)
+  `,
+  answerCode: `(* Start typing from the line below. *)
+  pragma solidity 0.6.3;
+
+contract Storage`
+};
+
+const l2: ILessonCode = {
+  initialCode: `scilla_version 0
+
+(* Insert the immutable variable declaration in parantheses after the contract name below *)
+contract SocialMediaPayment
+
+`,
+  answerCode: `scilla_version 0
+(* Insert the immutable variable declaration in parantheses after the contract name below *)
+contract SocialMediaPayment (owner: ByStr20)`
+};
+
+const l3: ILessonCode = {
+  initialCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+(* Start typing from the line below. *)
+`,
+  answerCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+(* Start typing from the line below. *)
+field username : String = "Alice"`
+};
+
+const l4: ILessonCode = {
+  initialCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+
+(* Start typing from the line below *)
+`,
+  answerCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+
+(* Start typing from the line below *)
+transition changeName()
+end`
+};
+
+const l5: ILessonCode = {
+  initialCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+
+transition changeName()
+    (* Start typing from the line below *)
+end`,
+  answerCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+
+transition changeName()
+    (* Start typing from the line below *)
+    newname = "Bob"
+end`
+};
+
+const l6: ILessonCode = {
+  initialCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+
+transition changeName()
+     newname = "Bob" (*Now that we’ll be having another line, don’t forget to include a semicolon at the end of of the previous line, i.e. after "Bob" *)
+    (* Start typing from the line below *)
+end`,
+  answerCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+
+transition changeName()
+     newname = "Bob"; (*Now that we’ll be having another line, don’t forget to include a semicolon at the end of the previous line, i.e. after "Bob" *)
+    (* Start typing from the line below *)
+    username := newname
+end`
+};
+
+const l7: ILessonCode = {
+  initialCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+
+
+(* Start typing in the parentheses below *)
+transition changeName()
+   username := newname 
+end
+`,
+  answerCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+
+(* Start typing in the parentheses below *)
+transition changeName(newname: String)
+    username := newname
+end`
+};
+
+const l8: ILessonCode = {
+  initialCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+(*Declare the two new mutable variables below. You don’t need to use any semicolons to separate the lines outside the transitions*)
+
+transition changeName(newname: String)
+    username := newname
+    (*Use ‘accept’ command in the line below which will accept the amount sent to this transition.*)
+
+    (*Assign the value of the implicit variables to the new mutable variables in the lines below. You’ll need to use the semicolons to separate the lines in the transition*)
+
+end
+`,
+  answerCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+field username : String = "Alice"
+field user_address : ByStr20 = 0x1234567890123456789012345678901234567890
+field  user_tokens: Uint128 = Uint128 0
+
+transition changeName(newname: String)
+    username := newname;
+    (*Use ‘accept’ command in the line below which will accept the amount sent to this transition.*)
+    accept; 
+    (*Assign the value of the implicit variables to the new mutable variables in the lines below. You’ll need to use the semicolons to separate the lines in the transition*)
+    user_address := _sender;
+    user_tokens := _amount
+
+end`
+};
+
+const l9: ILessonCode = {
+  initialCode: `contract Zealgame
+(owner: ByStr20)
+field player_name : String = "Alice"
+field player_address : ByStr20 = 0x1234567890123456789012345678901234567890
+field player_zeal: Uint128 = Uint128 0
+
+Transition changeName(newname: String)
+    player_name := newname;
+    player_address := _sender;
+    player_zeal := _amount
+end
+`,
+  answerCode: `contract Zealgame
+(owner: ByStr20)
+field player_name : String = "Alice"
+field player_address : ByStr20 = 0x1234567890123456789012345678901234567890
+field player_zeal: Uint128 = Uint128 0
+
+Transition changeName(newname: String)
+    player_name := newname;
+    player_address := _sender;
+    player_zeal := _amount
+end`
+};
+
+const chapter1: ChapterCodeType = [l1, l2, l3, l4, l5, l6, l7, l8, l9];
+
+export default chapter1;
---
+++ src/course-codes/solidity/chapter2/index.ts
@@ -0,0 +1,378 @@
+import { ILessonCode, ChapterCodeType } from '../../../typings';
+
+const l1: ILessonCode = {
+  initialCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+(*Start typing from the line below*)
+`,
+  answerCode: `scilla_version 0
+
+contract SocialMediaPayment (owner: ByStr20)
+(*Start typing from the line below*)
+
+transition deposit()
+
+end 
+`
+};
+
+const l2: ILessonCode = {
+  initialCode: `scilla_version 0
+
+(*Start typing from the line below*)
+
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+
+end
+`,
+  answerCode: `scilla_version 0
+
+(*Start typing from the line below*)
+import BoolUtils
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+
+end`
+};
+
+const l3: ILessonCode = {
+  initialCode: `scilla_version 0
+import BoolUtils
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+(*Start typing from the line below. The answer will be a single line within the transition so you won’t need to use semicolon at the end*)
+
+end
+`,
+  answerCode: `scilla_version 0
+  import BoolUtils
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+(*Start typing from the line below. The answer will be a single line within the transition so you won’t need to use semicolon at the end*)
+
+sender_is_owner = builtin eq _sender owner
+
+
+end`
+};
+
+const l4: ILessonCode = {
+  initialCode: `scilla_version 0
+import BoolUtils
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner
+(*Start typing from the line below. *)
+
+end
+`,
+  answerCode: `scilla_version 0
+import BoolUtils
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+(*Start typing from the line below. *)
+match sender_is_owner with
+     | False =>
+
+     | True =>
+
+     end
+end`
+};
+
+const l5: ILessonCode = {
+  initialCode: `scilla_version 0
+import BoolUtils
+(*Start typing from the line below. First define the library*)
+
+(*Now define the variable zero for Uint128 0*)
+
+
+(*Now, in the line below, define the variable not_owner_code of type Uint32 and value 1*)
+
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+
+     | True =>
+
+     end
+end
+`,
+  answerCode: `scilla_version 0
+import BoolUtils
+(*Start typing from the line below. First define the library*)
+library SocialMediaPayment
+
+(*Now define the variable zero for Uint128 0*)
+let zero = Uint128 0
+
+
+(*Now, in the line below, define the variable not_owner_code of type Uint32 and value 1*)
+let not_owner_code = Uint32 1
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+
+     | True =>
+
+     end
+end`
+};
+
+const l6: ILessonCode = {
+  initialCode: `scilla_version 0
+import BoolUtils
+
+library SocialMediaPayment
+(*Start typing from the line below. Copy paste the code given in the first task below to define the variable one_msg*)
+
+
+let zero = Uint128 0
+let not_owner_code = Uint32 1
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     (*Start typing from the line below. Define the variable msg below with the values given in the second point of the task*)
+
+
+    (*Start typing from the line below. Copy the two lines given in the third point of the task below. *)
+
+
+     | True =>
+
+     end
+end
+`,
+  answerCode: `scilla_version 0
+import BoolUtils
+
+library SocialMediaPayment
+(*Start typing from the line below. Copy paste the code given in the first task below to define the variable one_msg*)
+let one_msg = 
+  fun (msg : Message) => 
+  let nil_msg = Nil {Message} in
+  Cons {Message} msg nil_msg
+
+let zero = Uint128 0
+let not_owner_code = Uint32 1
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     (*Start typing from the line below. Define the variable msg below with the values given in the second point of the task*)
+
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+    (*Start typing from the line below. Copy the two lines given in the third point of the task below. *)
+          msgs = one_msg msg;
+          send msgs
+
+     | True =>
+
+     end
+end
+  `
+};
+
+const l7: ILessonCode = {
+  initialCode: `scilla_version 0
+import BoolUtils
+
+library SocialMediaPayment
+
+let one_msg = 
+  fun (msg : Message) => 
+  let nil_msg = Nil {Message} in
+  Cons {Message} msg nil_msg
+
+let zero = Uint128 0
+let not_owner_code = Uint32 1
+(*Start typing from the line below. declare a Uint32 type of variable with variable name “accepted_code” and value “0” *)
+
+
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+          msgs = one_msg msg;
+          send msgs
+
+     | True =>
+        (*Start typing from the line below for task 2, 3 and 4*)
+        
+        
+     end
+end
+`,
+  answerCode: `scilla_version 0
+import BoolUtils
+
+library SocialMediaPayment
+
+let one_msg = 
+  fun (msg : Message) => 
+  let nil_msg = Nil {Message} in
+  Cons {Message} msg nil_msg
+
+let zero = Uint128 0
+let not_owner_code = Uint32 1
+  (*Start typing from the line below. declare a Uint32 type of variable with variable name “accepted_code” and value “0” *)
+let accepted_code = Uint32 0
+
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+          msgs = one_msg msg;
+          send msgs
+
+     | True =>
+        (*Start typing from the line below for task 2, 3 and 4*)
+        accept;
+        msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: accepted_code};
+        msgs = one_msg msg;
+        send msgs
+        
+     end
+end
+  `
+};
+
+const l8: ILessonCode = {
+  initialCode: `scilla_version 0
+import BoolUtils
+
+library SocialMediaPayment
+
+let one_msg = 
+  fun (msg : Message) => 
+  let nil_msg = Nil {Message} in
+  Cons {Message} msg nil_msg
+
+let zero = Uint128 0
+let not_owner_code = Uint32 1
+(*Start typing from the line below. declare a Uint32 type of variable with variable name “accepted_code” and value “0” *)
+
+
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+          msgs = one_msg msg;
+          send msgs
+
+     | True =>
+        (*Start typing from the line below for task 2, 3 and 4*)
+        
+        
+     end
+end
+`,
+  answerCode: `scilla_version 0
+import BoolUtils
+
+library SocialMediaPayment
+
+let one_msg = 
+  fun (msg : Message) => 
+  let nil_msg = Nil {Message} in
+  Cons {Message} msg nil_msg
+
+let zero = Uint128 0
+let not_owner_code = Uint32 1
+  (*Start typing from the line below. declare a Uint32 type of variable with variable name “accepted_code” and value “0” *)
+let accepted_code = Uint32 0
+
+
+contract SocialMediaPayment(owner: ByStr20)
+
+transition deposit()
+sender_is_owner = builtin eq _sender owner;
+match sender_is_owner with
+     | False =>
+     
+          msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: not_owner_code};
+          msgs = one_msg msg;
+          send msgs
+
+     | True =>
+        (*Start typing from the line below for task 2, 3 and 4*)
+        accept;
+        msg = {_tag: "";
+                _recipient: _sender;
+                _amount: zero;
+                code: accepted_code};
+        msgs = one_msg msg;
+        send msgs
+        
+     end
+end
+  `
+};
+
+const chapter2: ChapterCodeType = [l1, l2, l3, l4, l5, l6, l7, l8];
+
+export default chapter2;
